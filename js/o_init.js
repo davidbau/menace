b18fb5dae19b9bc5711e2fd215e41189bd104334
David Bau <david.bau@gmail.com>
2026-03-09 08:29:38 -0400

    nhgetch: switch discoveries page-nav read from wrap to raw

--- js/o_init.js
+++ js/o_init.js
@@ -11,7 +11,7 @@ import { exercise } from './attrib_exercise.js';
 import { A_WIS } from './const.js';
 import { game as _gstate } from './gstate.js';
 import { resetIdentCounter, doname } from './mkobj.js';
-import { nhgetch_wrap } from './input.js';
+import { nhgetch_raw } from './input.js';
 import { awaitInput } from './suspend.js';
 import {
     objectData, initObjectData, bases,
@@ -682,7 +682,7 @@ export async function handleDiscoveries(game) {
     let pageIndex = 0;
     while (true) {
         await drawDiscoveriesPage(display, pages[pageIndex] || []);
-        const ch = await awaitInput(game, nhgetch_wrap(), { site: 'o_init.handleDiscoveries.pageNav' });
+        const ch = await awaitInput(game, nhgetch_raw(), { site: 'o_init.handleDiscoveries.pageNav' });
         if (ch === 32 || ch === 10 || ch === 13) {
             if (pageIndex + 1 < pages.length) {
                 pageIndex++;
